0af4fd6252e14618f412838c199c79ff53904424
Ben Slabbert <ben@hex.co.za>
2018-01-04 12:48:12 +0200

    adding intial reducer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import reducers from './reducers';
+
 export default class App extends Component {
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         hello
