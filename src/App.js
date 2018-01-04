dbe83b1e53aa7cbfe0d83022c77edb743a957cc2
Ben Slabbert <ben@hex.co.za>
2018-01-04 12:51:29 +0200

    firebase initialization

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import reducers from './reducers';
 
 export default class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCmSA8D9OkUBBATAp9O-8MUSQhWz4h2XLY',
+            authDomain: 'manager-bcc57.firebaseapp.com',
+            databaseURL: 'https://manager-bcc57.firebaseio.com',
+            projectId: 'manager-bcc57',
+            storageBucket: '',
+            messagingSenderId: '305636541708'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
