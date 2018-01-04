8b1ea3c9ff93a2419771acdf32adc9ef626fb05f
Ben Slabbert <ben@hex.co.za>
2018-01-04 17:28:46 +0200

    bringing in common components adding new login form with redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
 
@@ -24,9 +25,7 @@ export default class App extends Component {
         return (
             <Provider store={createStore(reducers)}>
                 <View>
-                    <Text>
-                        hello
-                    </Text>
+                    <LoginForm/>
                 </View>
             </Provider>
         );
