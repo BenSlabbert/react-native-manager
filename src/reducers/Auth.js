bca51098503875650ae97416622570736d7c1c7c
Ben Slabbert <ben@hex.co.za>
2018-01-05 16:43:37 +0200

    better way to reset state

--- src/reducers/Auth.js
+++ src/reducers/Auth.js
@@ -30,9 +30,8 @@ export default ( state = INITIAL_STATE, action ) => {
         case LOGIN_USER_SUCCESS:
             return {
                 ...state,
-                user: action.payload,
-                error: '',
-                loading: false
+                ...INITIAL_STATE,
+                user: action.payload
             };
 
         case LOGIN_USER_FAIL:
