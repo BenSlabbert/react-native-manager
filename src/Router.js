b6428ac5fa5104ae8fe31b2ed357536210b6257f
Ben Slabbert <ben@hex.co.za>
2018-01-07 09:45:03 +0200

    adding router with some routes

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Scene, Stack, Router } from 'react-native-router-flux';
+
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Stack key="root">
                 <Scene key="login" component={LoginForm} title="Login"/>
+                <Scene key="employeeList" component={EmployeeList} title="Employees"/>
             </Stack>
         </Router>
     );
